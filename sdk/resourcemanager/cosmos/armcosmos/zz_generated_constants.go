6f4bbb8c466d72cc61edae06aa652e666ddb5545
SDKAuto <sdkautomation@microsoft.com>
2022-04-21 15:12:16 +0000

    CodeGen from PR 18616 in Azure/azure-rest-api-specs Merge 2b74399bdea5b55abdaac69dbc203b35481ed753 into 3439439b760e84e5dcc3e68edde7251c602da5f0

--- sdk/resourcemanager/cosmos/armcosmos/zz_generated_constants.go
+++ sdk/resourcemanager/cosmos/armcosmos/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armcosmos
 
 const (
 	moduleName    = "armcosmos"
-	moduleVersion = "v0.5.0"
+	moduleVersion = "v0.5.1"
 )
 
 // APIType - Enum to indicate the API type of the restorable database account.
