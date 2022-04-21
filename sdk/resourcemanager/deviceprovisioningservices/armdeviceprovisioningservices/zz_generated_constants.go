b34bee624ea861df7463589614f6bd849d6f5535
SDKAuto <sdkautomation@microsoft.com>
2022-04-21 15:13:59 +0000

    CodeGen from PR 18616 in Azure/azure-rest-api-specs Merge 2b74399bdea5b55abdaac69dbc203b35481ed753 into 3439439b760e84e5dcc3e68edde7251c602da5f0

--- sdk/resourcemanager/deviceprovisioningservices/armdeviceprovisioningservices/zz_generated_constants.go
+++ sdk/resourcemanager/deviceprovisioningservices/armdeviceprovisioningservices/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armdeviceprovisioningservices
 
 const (
 	moduleName    = "armdeviceprovisioningservices"
-	moduleVersion = "v0.4.0"
+	moduleVersion = "v0.4.1"
 )
 
 // AccessRightsDescription - Rights that this key has.
