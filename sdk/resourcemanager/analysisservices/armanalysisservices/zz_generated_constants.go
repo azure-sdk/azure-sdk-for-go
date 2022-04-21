3c4da9b13949fa120eb7cf86aceabab35fdecf04
SDKAuto <sdkautomation@microsoft.com>
2022-04-21 15:22:21 +0000

    CodeGen from PR 18771 in Azure/azure-rest-api-specs Merge 2b74399bdea5b55abdaac69dbc203b35481ed753 into 3439439b760e84e5dcc3e68edde7251c602da5f0

--- sdk/resourcemanager/analysisservices/armanalysisservices/zz_generated_constants.go
+++ sdk/resourcemanager/analysisservices/armanalysisservices/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armanalysisservices
 
 const (
 	moduleName    = "armanalysisservices"
-	moduleVersion = "v0.4.0"
+	moduleVersion = "v0.4.1"
 )
 
 // ConnectionMode - How the read-write server's participation in the query pool is controlled.
