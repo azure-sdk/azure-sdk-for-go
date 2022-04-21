4955cb13b45dedd0dae67c83841ad6b36335cae2
SDKAuto <sdkautomation@microsoft.com>
2022-04-21 15:16:11 +0000

    CodeGen from PR 18616 in Azure/azure-rest-api-specs Merge 2b74399bdea5b55abdaac69dbc203b35481ed753 into 3439439b760e84e5dcc3e68edde7251c602da5f0

--- sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/zz_generated_constants.go
+++ sdk/resourcemanager/hybridkubernetes/armhybridkubernetes/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armhybridkubernetes
 
 const (
 	moduleName    = "armhybridkubernetes"
-	moduleVersion = "v0.4.0"
+	moduleVersion = "v0.4.1"
 )
 
 // AuthenticationMethod - The mode of client authentication.
