5a388ebafb8a2f0655619c8db50e9d370f3cc181
SDKAuto <sdkautomation@microsoft.com>
2022-05-24 00:07:40 +0000

    CodeGen from PR 19189 in Azure/azure-rest-api-specs Merge 583c1df46edc8db6d87dacfac282e4f29308c3d4 into 92cf370fd73318f3cf92c36403274ba695873857

--- sdk/resourcemanager/sqlvirtualmachine/armsqlvirtualmachine/zz_generated_constants.go
+++ sdk/resourcemanager/sqlvirtualmachine/armsqlvirtualmachine/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armsqlvirtualmachine
 
 const (
 	moduleName    = "armsqlvirtualmachine"
-	moduleVersion = "v0.5.0"
+	moduleVersion = "v0.5.1"
 )
 
 // BackupScheduleType - Backup schedule type.
