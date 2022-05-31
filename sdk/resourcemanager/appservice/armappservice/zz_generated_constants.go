35aefbeab8334093216df1ce6889c3e711be6596
SDKAuto <sdkautomation@microsoft.com>
2022-05-31 08:17:15 +0000

    CodeGen from PR 19279 in Azure/azure-rest-api-specs Merge d945a5809dbf350176665da256c4d7bf96a77c69 into ef2dedc629c54795597b7e11affad0cd6e804a8b

--- sdk/resourcemanager/appservice/armappservice/zz_generated_constants.go
+++ sdk/resourcemanager/appservice/armappservice/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armappservice
 
 const (
 	moduleName    = "armappservice"
-	moduleVersion = "v1.0.0"
+	moduleVersion = "v1.0.1"
 )
 
 // ActiveRevisionsMode - ActiveRevisionsMode controls how active revisions are handled for the Container app:Multiple: multiple
