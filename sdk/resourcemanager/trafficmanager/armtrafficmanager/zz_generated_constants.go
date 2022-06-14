5ce0e3ba4c67bfdb1c642cb4d23af4fe622897cc
SDKAuto <sdkautomation@microsoft.com>
2022-06-14 01:15:22 +0000

    CodeGen from PR 19142 in Azure/azure-rest-api-specs Merge 14dab7ffa01845d3dc6e2ecc692a252e06c56469 into de400f7204d30d25543ac967636180728d52a88f

--- sdk/resourcemanager/trafficmanager/armtrafficmanager/zz_generated_constants.go
+++ sdk/resourcemanager/trafficmanager/armtrafficmanager/zz_generated_constants.go
@@ -10,7 +10,7 @@ package armtrafficmanager
 
 const (
 	moduleName    = "armtrafficmanager"
-	moduleVersion = "v1.0.0"
+	moduleVersion = "v1.0.1"
 )
 
 // AllowedEndpointRecordType - The allowed type DNS record types for this profile.
