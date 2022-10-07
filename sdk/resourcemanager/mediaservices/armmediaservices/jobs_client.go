fb2c2c76c7cf54df537c707e81888acf4ec2e7d8
SDKAuto <sdkautomation@microsoft.com>
2022-10-07 23:01:08 +0000

    CodeGen from PR 21035 in Azure/azure-rest-api-specs Merge 406c99992edd6bd7616d5795b39428b0e3e4d59f into 886cb1c95499e7577e14ba1b4317f63590d79992

--- sdk/resourcemanager/mediaservices/armmediaservices/jobs_client.go
+++ sdk/resourcemanager/mediaservices/armmediaservices/jobs_client.go
@@ -303,7 +303,6 @@ func (client *JobsClient) getHandleResponse(resp *http.Response) (JobsClientGetR
 }
 
 // NewListPager - Lists all of the Jobs for the Transform.
-// If the operation fails it returns an *azcore.ResponseError type.
 // Generated from API version 2021-11-01
 // resourceGroupName - The name of the resource group within the Azure subscription.
 // accountName - The Media Services account name.
