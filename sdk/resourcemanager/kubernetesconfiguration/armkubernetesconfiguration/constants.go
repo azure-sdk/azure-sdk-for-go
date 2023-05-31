07727d68e2aaa182749ce0dfcdb9b9b99a691d3a
SDKAuto <sdkautomation@microsoft.com>
2023-05-31 10:27:23 +0000

    CodeGen from PR 2753 in test-repo-billy/azure-rest-api-specs Merge d4138083c1b04e801f5ec64d7e27076cdef39dee into ba1184c32a778948ea8836c9143821a874dcfcac

--- sdk/resourcemanager/kubernetesconfiguration/armkubernetesconfiguration/constants.go
+++ sdk/resourcemanager/kubernetesconfiguration/armkubernetesconfiguration/constants.go
@@ -11,7 +11,7 @@ package armkubernetesconfiguration
 
 const (
 	moduleName    = "armkubernetesconfiguration"
-	moduleVersion = "v2.0.0"
+	moduleVersion = "v2.1.0"
 )
 
 // AKSIdentityType - The identity type.
