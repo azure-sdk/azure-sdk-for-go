b1379a13cb9fe514057aa1ca0bec5610685dac69
SDKAuto <sdkautomation@microsoft.com>
2023-06-29 00:25:36 +0000

    CodeGen from PR 24631 in Azure/azure-rest-api-specs Merge 604804858d8b1b1074164e84c56eeac16941f407 into b133da476a0204b8ba47eb4bec15db5e182d6fed

--- sdk/resourcemanager/sql/armsql/serveroperations_client.go
+++ sdk/resourcemanager/sql/armsql/serveroperations_client.go
@@ -46,7 +46,7 @@ func NewServerOperationsClient(subscriptionID string, credential azcore.TokenCre
 
 // NewListByServerPager - Gets a list of operations performed on the server.
 //
-// Generated from API version 2020-11-01-preview
+// Generated from API version 2022-11-01-preview
 //   - resourceGroupName - The name of the resource group that contains the resource. You can obtain this value from the Azure
 //     Resource Manager API or the portal.
 //   - serverName - The name of the server.
@@ -100,7 +100,7 @@ func (client *ServerOperationsClient) listByServerCreateRequest(ctx context.Cont
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2020-11-01-preview")
+	reqQP.Set("api-version", "2022-11-01-preview")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
