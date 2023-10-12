025b4504fb9828fddd49b1f7858b4ac0d28c2f9f
SDKAuto <sdkautomation@microsoft.com>
2023-10-12 04:07:23 +0000

    CodeGen from PR 26172 in Azure/azure-rest-api-specs Merge 5cace91d5595747a200c202cbff811d24bcf51aa into 43c5786b7d90124e46fe068894694ac899965948

--- sdk/resourcemanager/recoveryservices/armrecoveryservicesbackup/constants.go
+++ sdk/resourcemanager/recoveryservices/armrecoveryservicesbackup/constants.go
@@ -10,7 +10,7 @@ package armrecoveryservicesbackup
 
 const (
 	moduleName    = "armrecoveryservicesbackup"
-	moduleVersion = "v3.0.0"
+	moduleVersion = "v4.0.0"
 )
 
 // AcquireStorageAccountLock - Whether storage account lock is to be acquired for this container or not.
