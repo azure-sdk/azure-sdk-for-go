2bf8604ae11a658684e4f16b3409780f5dd35e10
SDKAuto <sdkautomation@microsoft.com>
2023-10-31 04:27:01 +0000

    CodeGen from PR 26427 in Azure/azure-rest-api-specs Merge ca53b46a98fe227af975a016ddb76898b2040482 into ceffc2870986ab280d7ee7e056297ca5b449abed

--- sdk/resourcemanager/containerservice/armcontainerservice/client_factory.go
+++ sdk/resourcemanager/containerservice/armcontainerservice/client_factory.go
@@ -23,7 +23,7 @@ type ClientFactory struct {
 
 // NewClientFactory creates a new instance of ClientFactory with the specified values.
 // The parameter values will be propagated to any client created from this factory.
-//   - subscriptionID - The ID of the target subscription.
+//   - subscriptionID - The ID of the target subscription. The value must be an UUID.
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
 func NewClientFactory(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*ClientFactory, error) {
@@ -42,18 +42,8 @@ func (c *ClientFactory) NewAgentPoolsClient() *AgentPoolsClient {
 	return subClient
 }
 
-func (c *ClientFactory) NewMachinesClient() *MachinesClient {
-	subClient, _ := NewMachinesClient(c.subscriptionID, c.credential, c.options)
-	return subClient
-}
-
 func (c *ClientFactory) NewMaintenanceConfigurationsClient() *MaintenanceConfigurationsClient {
 	subClient, _ := NewMaintenanceConfigurationsClient(c.subscriptionID, c.credential, c.options)
-	return subClient
-}
-
-func (c *ClientFactory) NewManagedClusterSnapshotsClient() *ManagedClusterSnapshotsClient {
-	subClient, _ := NewManagedClusterSnapshotsClient(c.subscriptionID, c.credential, c.options)
 	return subClient
 }
 
