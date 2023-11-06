d2c8a249da6b07d489253e601b9a5766136b265e
SDKAuto <sdkautomation@microsoft.com>
2023-11-06 22:33:17 +0000

    CodeGen from PR 26203 in Azure/azure-rest-api-specs Merge 6aaa945505c71a9e23084230c48cc98ac40f9fb3 into b594fd4145904c9bbd018f4a2ddea75c7134aee7

--- sdk/resourcemanager/containerservicefleet/armcontainerservicefleet/constants.go
+++ sdk/resourcemanager/containerservicefleet/armcontainerservicefleet/constants.go
@@ -10,7 +10,7 @@ package armcontainerservicefleet
 
 const (
 	moduleName    = "armcontainerservicefleet"
-	moduleVersion = "v1.0.0"
+	moduleVersion = "v1.0.1"
 )
 
 // ActionType - Enum. Indicates the action type. "Internal" refers to actions that are for internal only APIs.
