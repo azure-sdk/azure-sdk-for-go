0fa9f0511dbc6bc99490d4c1f6d259e17d44b2a8
SDKAuto <sdkautomation@microsoft.com>
2023-12-08 10:12:19 +0000

    CodeGen from PR 26946 in Azure/azure-rest-api-specs Merge b08d1751dbe10ac5619938962b87ac8b4fb4e66d into 4e8862afb891a4c7835a227a1407898ae6e25985

--- sdk/resourcemanager/appplatform/armappplatform/fake/deployments_server.go
+++ sdk/resourcemanager/appplatform/armappplatform/fake/deployments_server.go
@@ -677,10 +677,16 @@ func (d *DeploymentsServerTransport) dispatchNewListPager(req *http.Request) (*h
 			}
 			versionParam[i] = u
 		}
+		expandUnescaped, err := url.QueryUnescape(qp.Get("$expand"))
+		if err != nil {
+			return nil, err
+		}
+		expandParam := getOptional(expandUnescaped)
 		var options *armappplatform.DeploymentsClientListOptions
-		if len(versionParam) > 0 {
+		if len(versionParam) > 0 || expandParam != nil {
 			options = &armappplatform.DeploymentsClientListOptions{
 				Version: versionParam,
+				Expand:  expandParam,
 			}
 		}
 		resp := d.srv.NewListPager(resourceGroupNameParam, serviceNameParam, appNameParam, options)
@@ -734,10 +740,16 @@ func (d *DeploymentsServerTransport) dispatchNewListForClusterPager(req *http.Re
 			}
 			versionParam[i] = u
 		}
+		expandUnescaped, err := url.QueryUnescape(qp.Get("$expand"))
+		if err != nil {
+			return nil, err
+		}
+		expandParam := getOptional(expandUnescaped)
 		var options *armappplatform.DeploymentsClientListForClusterOptions
-		if len(versionParam) > 0 {
+		if len(versionParam) > 0 || expandParam != nil {
 			options = &armappplatform.DeploymentsClientListForClusterOptions{
 				Version: versionParam,
+				Expand:  expandParam,
 			}
 		}
 		resp := d.srv.NewListForClusterPager(resourceGroupNameParam, serviceNameParam, options)
