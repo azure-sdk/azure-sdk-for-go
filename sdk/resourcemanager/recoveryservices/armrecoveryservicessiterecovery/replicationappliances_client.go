e69939d199693b407d50f77875847a347e82c25b
SDKAuto <sdkautomation@microsoft.com>
2024-01-11 12:20:26 +0000

    CodeGen from PR 26302 in Azure/azure-rest-api-specs Merge e5e02179b3555cab605c8a62196884f5fe3c42a0 into ea28180c6ce9027df36568307f235868d581144c

--- sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/replicationappliances_client.go
+++ sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/replicationappliances_client.go
@@ -45,7 +45,7 @@ func NewReplicationAppliancesClient(subscriptionID string, credential azcore.Tok
 
 // NewListPager - Gets the list of Azure Site Recovery appliances for the vault.
 //
-// Generated from API version 2023-06-01
+// Generated from API version 2023-08-01
 //   - resourceName - The name of the recovery services vault.
 //   - resourceGroupName - The name of the resource group where the recovery services vault is present.
 //   - options - ReplicationAppliancesClientListOptions contains the optional parameters for the ReplicationAppliancesClient.NewListPager
@@ -93,7 +93,7 @@ func (client *ReplicationAppliancesClient) listCreateRequest(ctx context.Context
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2023-06-01")
+	reqQP.Set("api-version", "2023-08-01")
 	if options != nil && options.Filter != nil {
 		reqQP.Set("$filter", *options.Filter)
 	}
--- sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/replicationprotectableitems_client.go
+++ sdk/resourcemanager/recoveryservices/armrecoveryservicessiterecovery/replicationprotectableitems_client.go
@@ -46,7 +46,7 @@ func NewReplicationProtectableItemsClient(subscriptionID string, credential azco
 // Get - The operation to get the details of a protectable item.
 // If the operation fails it returns an *azcore.ResponseError type.
 //
-// Generated from API version 2023-06-01
+// Generated from API version 2023-08-01
 //   - resourceName - The name of the recovery services vault.
 //   - resourceGroupName - The name of the resource group where the recovery services vault is present.
 //   - fabricName - Fabric name.
@@ -108,7 +108,7 @@ func (client *ReplicationProtectableItemsClient) getCreateRequest(ctx context.Co
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2023-06-01")
+	reqQP.Set("api-version", "2023-08-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
@@ -125,7 +125,7 @@ func (client *ReplicationProtectableItemsClient) getHandleResponse(resp *http.Re
 
 // NewListByReplicationProtectionContainersPager - Lists the protectable items in a protection container.
 //
-// Generated from API version 2023-06-01
+// Generated from API version 2023-08-01
 //   - resourceName - The name of the recovery services vault.
 //   - resourceGroupName - The name of the resource group where the recovery services vault is present.
 //   - fabricName - Fabric name.
@@ -183,7 +183,7 @@ func (client *ReplicationProtectableItemsClient) listByReplicationProtectionCont
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2023-06-01")
+	reqQP.Set("api-version", "2023-08-01")
 	if options != nil && options.Filter != nil {
 		reqQP.Set("$filter", *options.Filter)
 	}
