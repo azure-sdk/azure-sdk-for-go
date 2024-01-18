055f019b6a2957e53c5ac0e519797bac9932d977
SDKAuto <sdkautomation@microsoft.com>
2024-01-18 16:16:18 +0000

    CodeGen from PR 25559 in Azure/azure-rest-api-specs Merge 5533b2fa3c6334fc8fb166972d5b502f35834032 into 8b5618d760532b69d6f7434f57172ea52e109c79

--- sdk/resourcemanager/resources/armresources/models.go
+++ sdk/resourcemanager/resources/armresources/models.go
@@ -86,6 +86,33 @@ type BasicDependency struct {
 	ResourceType *string
 }
 
+// DataBoundaryDefinition - A data boundary definition.
+type DataBoundaryDefinition struct {
+	// The data boundary definition.
+	DataBoundary *DataBoundary
+
+	// Resource extended location.
+	ExtendedLocation *ExtendedLocation
+
+	// Resource location
+	Location *string
+
+	// Resource tags
+	Tags map[string]*string
+
+	// READ-ONLY; Resource ID
+	ID *string
+
+	// READ-ONLY; Resource name
+	Name *string
+
+	// READ-ONLY; Denotes the state of provisioning.
+	ProvisioningState *ProvisioningState
+
+	// READ-ONLY; Resource type
+	Type *string
+}
+
 // DebugSetting - The debug setting.
 type DebugSetting struct {
 	// Specifies the type of information to log for debugging. The permitted values are none, requestContent, responseContent,
@@ -221,6 +248,15 @@ type DeploymentOperationsListResult struct {
 	NextLink *string
 }
 
+// DeploymentParameter - Deployment parameter for the template.
+type DeploymentParameter struct {
+	// Azure Key Vault parameter reference.
+	Reference *KeyVaultParameterReference
+
+	// Input value to the parameter .
+	Value any
+}
+
 // DeploymentProperties - Deployment properties.
 type DeploymentProperties struct {
 	// REQUIRED; The mode that is used to deploy resources. This value can be either Incremental or Complete. In Incremental mode,
@@ -244,7 +280,7 @@ type DeploymentProperties struct {
 	// the parameter values directly in the request rather than link to an existing
 	// parameter file. Use either the parametersLink property or the parameters property, but not both. It can be a JObject or
 	// a well formed JSON string.
-	Parameters any
+	Parameters map[string]*DeploymentParameter
 
 	// The URI of parameters file. You use this element to link to an existing parameters file. Use either the parametersLink
 	// property or the parameters property, but not both.
@@ -354,7 +390,7 @@ type DeploymentWhatIfProperties struct {
 	// the parameter values directly in the request rather than link to an existing
 	// parameter file. Use either the parametersLink property or the parameters property, but not both. It can be a JObject or
 	// a well formed JSON string.
-	Parameters any
+	Parameters map[string]*DeploymentParameter
 
 	// The URI of parameters file. You use this element to link to an existing parameters file. Use either the parametersLink
 	// property or the parameters property, but not both.
@@ -561,6 +597,24 @@ type IdentityUserAssignedIdentitiesValue struct {
 
 	// READ-ONLY; The principal id of user assigned identity.
 	PrincipalID *string
+}
+
+// KeyVaultParameterReference - Azure Key Vault parameter reference.
+type KeyVaultParameterReference struct {
+	// REQUIRED; Azure Key Vault reference.
+	KeyVault *KeyVaultReference
+
+	// REQUIRED; Azure Key Vault secret name.
+	SecretName *string
+
+	// Azure Key Vault secret version.
+	SecretVersion *string
+}
+
+// KeyVaultReference - Azure Key Vault reference.
+type KeyVaultReference struct {
+	// REQUIRED; Azure Key Vault resource id.
+	ID *string
 }
 
 // MoveInfo - Parameters of move resources.
