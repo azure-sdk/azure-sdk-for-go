22577c9efa4f375e75757802218fef815087271d
SDKAuto <sdkautomation@microsoft.com>
2024-02-13 05:27:16 +0000

    CodeGen from PR 27756 in Azure/azure-rest-api-specs Merge 2c547d7c308fc18548979a0412de09c6e5fa3259 into 0c885c14263db5ebfe87e45c767d44a16f086a43

--- sdk/resourcemanager/confluent/armconfluent/client_factory.go
+++ sdk/resourcemanager/confluent/armconfluent/client_factory.go
@@ -23,7 +23,7 @@ type ClientFactory struct {
 
 // NewClientFactory creates a new instance of ClientFactory with the specified values.
 // The parameter values will be propagated to any client created from this factory.
-//   - subscriptionID - The ID of the target subscription. The value must be an UUID.
+//   - subscriptionID - Microsoft Azure subscription id
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
 func NewClientFactory(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*ClientFactory, error) {
