2f05877967cfe2a4e978f169dfa3dc6ca3302ee0
SDKAuto <sdkautomation@microsoft.com>
2024-03-15 22:18:25 +0000

    CodeGen from PR 28288 in Azure/azure-rest-api-specs Merge 28b29ba67e9464e9f1e7d262d10442109705d8aa into a7f8fb628797436ba442c2b59afb082a4080a9cd

--- sdk/resourcemanager/desktopvirtualization/armdesktopvirtualization/startmenuitems_client.go
+++ sdk/resourcemanager/desktopvirtualization/armdesktopvirtualization/startmenuitems_client.go
@@ -46,7 +46,7 @@ func NewStartMenuItemsClient(subscriptionID string, credential azcore.TokenCrede
 
 // NewListPager - List start menu items in the given application group.
 //
-// Generated from API version 2023-09-05
+// Generated from API version 2024-03-06-preview
 //   - resourceGroupName - The name of the resource group. The name is case insensitive.
 //   - applicationGroupName - The name of the application group
 //   - options - StartMenuItemsClientListOptions contains the optional parameters for the StartMenuItemsClient.NewListPager method.
@@ -93,15 +93,15 @@ func (client *StartMenuItemsClient) listCreateRequest(ctx context.Context, resou
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2023-09-05")
-	if options != nil && options.PageSize != nil {
-		reqQP.Set("pageSize", strconv.FormatInt(int64(*options.PageSize), 10))
+	reqQP.Set("api-version", "2024-03-06-preview")
+	if options != nil && options.InitialSkip != nil {
+		reqQP.Set("initialSkip", strconv.FormatInt(int64(*options.InitialSkip), 10))
 	}
 	if options != nil && options.IsDescending != nil {
 		reqQP.Set("isDescending", strconv.FormatBool(*options.IsDescending))
 	}
-	if options != nil && options.InitialSkip != nil {
-		reqQP.Set("initialSkip", strconv.FormatInt(int64(*options.InitialSkip), 10))
+	if options != nil && options.PageSize != nil {
+		reqQP.Set("pageSize", strconv.FormatInt(int64(*options.PageSize), 10))
 	}
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
