a1ed41509317545547a7ff85812105bf44b024ba
SDKAuto <sdkautomation@microsoft.com>
2024-03-19 03:16:32 +0000

    CodeGen from PR 3240 in test-repo-billy/azure-rest-api-specs Merge 3e7b7f30fd19ab35d922a94329c5d8047705de7f into f82646fc1477ba3bfd90478e044e01bef3b8831a

--- sdk/resourcemanager/servicelinker/armservicelinker/CHANGELOG.md
+++ sdk/resourcemanager/servicelinker/armservicelinker/CHANGELOG.md
@@ -1,5 +1,23 @@
 # Release History
 
+## 2.0.0 (2024-03-19)
+### Breaking Changes
+
+- Enum `ActionType` has been removed
+- Enum `Origin` has been removed
+- Function `*ClientFactory.NewOperationsClient` has been removed
+- Function `*LinkerClient.NewListPager` has been removed
+- Function `NewOperationsClient` has been removed
+- Function `*OperationsClient.NewListPager` has been removed
+- Struct `Operation` has been removed
+- Struct `OperationDisplay` has been removed
+- Struct `OperationListResult` has been removed
+
+### Features Added
+
+- New value `ClientTypeKafkaSpringBoot` added to enum type `ClientType`
+
+
 ## 1.2.0 (2023-11-24)
 ### Features Added
 
--- sdk/resourcemanager/servicelinker/armservicelinker/fake/server_factory.go
+++ sdk/resourcemanager/servicelinker/armservicelinker/fake/server_factory.go
@@ -19,8 +19,7 @@ import (
 
 // ServerFactory is a fake server for instances of the armservicelinker.ClientFactory type.
 type ServerFactory struct {
-	LinkerServer     LinkerServer
-	OperationsServer OperationsServer
+	LinkerServer LinkerServer
 }
 
 // NewServerFactoryTransport creates a new instance of ServerFactoryTransport with the provided implementation.
@@ -35,10 +34,9 @@ func NewServerFactoryTransport(srv *ServerFactory) *ServerFactoryTransport {
 // ServerFactoryTransport connects instances of armservicelinker.ClientFactory to instances of ServerFactory.
 // Don't use this type directly, use NewServerFactoryTransport instead.
 type ServerFactoryTransport struct {
-	srv                *ServerFactory
-	trMu               sync.Mutex
-	trLinkerServer     *LinkerServerTransport
-	trOperationsServer *OperationsServerTransport
+	srv            *ServerFactory
+	trMu           sync.Mutex
+	trLinkerServer *LinkerServerTransport
 }
 
 // Do implements the policy.Transporter interface for ServerFactoryTransport.
@@ -57,9 +55,6 @@ func (s *ServerFactoryTransport) Do(req *http.Request) (*http.Response, error) {
 	case "LinkerClient":
 		initServer(s, &s.trLinkerServer, func() *LinkerServerTransport { return NewLinkerServerTransport(&s.srv.LinkerServer) })
 		resp, err = s.trLinkerServer.Do(req)
-	case "OperationsClient":
-		initServer(s, &s.trOperationsServer, func() *OperationsServerTransport { return NewOperationsServerTransport(&s.srv.OperationsServer) })
-		resp, err = s.trOperationsServer.Do(req)
 	default:
 		err = fmt.Errorf("unhandled client %s", client)
 	}
