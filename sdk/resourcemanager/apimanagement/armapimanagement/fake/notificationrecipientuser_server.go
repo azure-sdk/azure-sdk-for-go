3f3887ef9fc5fe310c3e6b6d20e1670905693ba2
SDKAuto <sdkautomation@microsoft.com>
2024-06-07 20:07:57 +0000

    CodeGen from PR 29365 in Azure/azure-rest-api-specs Merge e089ef9e55e163e65e92d7ca0c06bb97ae2cbfae into f07c71dbb29b1338b728c7920dcfd97e085b739a

--- sdk/resourcemanager/apimanagement/armapimanagement/fake/notificationrecipientuser_server.go
+++ sdk/resourcemanager/apimanagement/armapimanagement/fake/notificationrecipientuser_server.go
@@ -15,7 +15,7 @@ import (
 	azfake "github.com/Azure/azure-sdk-for-go/sdk/azcore/fake"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/fake/server"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/apimanagement/armapimanagement/v3"
 	"net/http"
 	"net/url"
 	"regexp"
