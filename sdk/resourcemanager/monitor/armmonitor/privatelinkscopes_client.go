f54ae3fade95844dc7ce96bc67a58ec56299b6fc
SDKAuto <sdkautomation@microsoft.com>
2024-06-14 07:51:30 +0000

    CodeGen from PR 29440 in Azure/azure-rest-api-specs Merge 495ee87e131eed7c14f3033a79d38449b223307a into 7bf13ef1c776f6d973b9645906281d0ec4660fc8

--- sdk/resourcemanager/monitor/armmonitor/privatelinkscopes_client.go
+++ sdk/resourcemanager/monitor/armmonitor/privatelinkscopes_client.go
@@ -28,7 +28,7 @@ type PrivateLinkScopesClient struct {
 }
 
 // NewPrivateLinkScopesClient creates a new instance of PrivateLinkScopesClient with the specified values.
-//   - subscriptionID - The ID of the target subscription.
+//   - subscriptionID - The ID of the target subscription. The value must be an UUID.
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
 func NewPrivateLinkScopesClient(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*PrivateLinkScopesClient, error) {
