d4eb28daff8a745e3dd27c56d1697a6f234fa4f2
SDKAuto <sdkautomation@microsoft.com>
2024-07-17 20:05:32 +0000

    CodeGen from PR 29863 in Azure/azure-rest-api-specs Merge 5f3e64b9b2d371d2d0faefcb0e07deb639337732 into 5274dcc3d30158bde11d2f67b389833c933dec40

--- sdk/resourcemanager/monitor/armmonitor/alertruleincidents_client.go
+++ sdk/resourcemanager/monitor/armmonitor/alertruleincidents_client.go
@@ -28,7 +28,7 @@ type AlertRuleIncidentsClient struct {
 }
 
 // NewAlertRuleIncidentsClient creates a new instance of AlertRuleIncidentsClient with the specified values.
-//   - subscriptionID - The ID of the target subscription.
+//   - subscriptionID - The ID of the target subscription. The value must be an UUID.
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
 func NewAlertRuleIncidentsClient(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*AlertRuleIncidentsClient, error) {
