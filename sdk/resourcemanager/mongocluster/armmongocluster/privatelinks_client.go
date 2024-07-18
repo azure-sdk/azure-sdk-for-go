38e29da6b661bb76d7b58252338db3cc4fdab9cd
SDKAuto <sdkautomation@microsoft.com>
2024-07-18 09:42:47 +0000

    CodeGen from PR 3400 in test-repo-billy/azure-rest-api-specs Merge 52a1cb673e76ab825f13a43bd1f633995a0df2d0 into 1d5ee99f692e6fc95bc7bb3b463d6fb8f199b5e4

--- sdk/resourcemanager/mongocluster/armmongocluster/privatelinks_client.go
+++ sdk/resourcemanager/mongocluster/armmongocluster/privatelinks_client.go
@@ -39,38 +39,37 @@ func NewPrivateLinksClient(subscriptionID string, credential azcore.TokenCredent
 	return client, nil
 }
 
-// NewListByMongoClusterPager - list private links on the given resource
+// NewListPager - list private links on the given resource
 //
 // Generated from API version 2024-03-01-preview
 //   - resourceGroupName - The name of the resource group. The name is case insensitive.
 //   - mongoClusterName - The name of the mongo cluster.
-//   - options - PrivateLinksClientListByMongoClusterOptions contains the optional parameters for the PrivateLinksClient.NewListByMongoClusterPager
-//     method.
-func (client *PrivateLinksClient) NewListByMongoClusterPager(resourceGroupName string, mongoClusterName string, options *PrivateLinksClientListByMongoClusterOptions) *runtime.Pager[PrivateLinksClientListByMongoClusterResponse] {
-	return runtime.NewPager(runtime.PagingHandler[PrivateLinksClientListByMongoClusterResponse]{
-		More: func(page PrivateLinksClientListByMongoClusterResponse) bool {
+//   - options - PrivateLinksClientListOptions contains the optional parameters for the PrivateLinksClient.NewListPager method.
+func (client *PrivateLinksClient) NewListPager(resourceGroupName string, mongoClusterName string, options *PrivateLinksClientListOptions) *runtime.Pager[PrivateLinksClientListResponse] {
+	return runtime.NewPager(runtime.PagingHandler[PrivateLinksClientListResponse]{
+		More: func(page PrivateLinksClientListResponse) bool {
 			return page.NextLink != nil && len(*page.NextLink) > 0
 		},
-		Fetcher: func(ctx context.Context, page *PrivateLinksClientListByMongoClusterResponse) (PrivateLinksClientListByMongoClusterResponse, error) {
-			ctx = context.WithValue(ctx, runtime.CtxAPINameKey{}, "PrivateLinksClient.NewListByMongoClusterPager")
+		Fetcher: func(ctx context.Context, page *PrivateLinksClientListResponse) (PrivateLinksClientListResponse, error) {
+			ctx = context.WithValue(ctx, runtime.CtxAPINameKey{}, "PrivateLinksClient.NewListPager")
 			nextLink := ""
 			if page != nil {
 				nextLink = *page.NextLink
 			}
 			resp, err := runtime.FetcherForNextLink(ctx, client.internal.Pipeline(), nextLink, func(ctx context.Context) (*policy.Request, error) {
-				return client.listByMongoClusterCreateRequest(ctx, resourceGroupName, mongoClusterName, options)
+				return client.listCreateRequest(ctx, resourceGroupName, mongoClusterName, options)
 			}, nil)
 			if err != nil {
-				return PrivateLinksClientListByMongoClusterResponse{}, err
+				return PrivateLinksClientListResponse{}, err
 			}
-			return client.listByMongoClusterHandleResponse(resp)
+			return client.listHandleResponse(resp)
 		},
 		Tracer: client.internal.Tracer(),
 	})
 }
 
-// listByMongoClusterCreateRequest creates the ListByMongoCluster request.
-func (client *PrivateLinksClient) listByMongoClusterCreateRequest(ctx context.Context, resourceGroupName string, mongoClusterName string, _ *PrivateLinksClientListByMongoClusterOptions) (*policy.Request, error) {
+// listCreateRequest creates the List request.
+func (client *PrivateLinksClient) listCreateRequest(ctx context.Context, resourceGroupName string, mongoClusterName string, _ *PrivateLinksClientListOptions) (*policy.Request, error) {
 	urlPath := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.DocumentDB/mongoClusters/{mongoClusterName}/privateLinkResources"
 	if client.subscriptionID == "" {
 		return nil, errors.New("parameter client.subscriptionID cannot be empty")
@@ -95,11 +94,11 @@ func (client *PrivateLinksClient) listByMongoClusterCreateRequest(ctx context.Co
 	return req, nil
 }
 
-// listByMongoClusterHandleResponse handles the ListByMongoCluster response.
-func (client *PrivateLinksClient) listByMongoClusterHandleResponse(resp *http.Response) (PrivateLinksClientListByMongoClusterResponse, error) {
-	result := PrivateLinksClientListByMongoClusterResponse{}
+// listHandleResponse handles the List response.
+func (client *PrivateLinksClient) listHandleResponse(resp *http.Response) (PrivateLinksClientListResponse, error) {
+	result := PrivateLinksClientListResponse{}
 	if err := runtime.UnmarshalAsJSON(resp, &result.PrivateLinkResourceListResult); err != nil {
-		return PrivateLinksClientListByMongoClusterResponse{}, err
+		return PrivateLinksClientListResponse{}, err
 	}
 	return result, nil
 }
