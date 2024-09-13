26f5e124bd1315107bd731bd6d7487e95c097fd0
SDKAuto <sdkautomation@microsoft.com>
2024-09-13 00:22:29 +0000

    CodeGen from PR 30497 in Azure/azure-rest-api-specs Merge d92c15fbe64813b2e2ef070ec74b7ba5782717cb into 2d614982e89de434fd8a7d96e7080c1d87271037

--- sdk/resourcemanager/servicebus/armservicebus/constants.go
+++ sdk/resourcemanager/servicebus/armservicebus/constants.go
@@ -10,7 +10,7 @@ package armservicebus
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/servicebus/armservicebus"
-	moduleVersion = "v1.2.0"
+	moduleVersion = "v1.3.0"
 )
 
 type AccessRights string
@@ -238,6 +238,24 @@ func PossibleProvisioningStateDRValues() []ProvisioningStateDR {
 	}
 }
 
+// PublicNetworkAccess - This determines if traffic is allowed over public network. By default it is enabled.
+type PublicNetworkAccess string
+
+const (
+	PublicNetworkAccessDisabled           PublicNetworkAccess = "Disabled"
+	PublicNetworkAccessEnabled            PublicNetworkAccess = "Enabled"
+	PublicNetworkAccessSecuredByPerimeter PublicNetworkAccess = "SecuredByPerimeter"
+)
+
+// PossiblePublicNetworkAccessValues returns the possible values for the PublicNetworkAccess const type.
+func PossiblePublicNetworkAccessValues() []PublicNetworkAccess {
+	return []PublicNetworkAccess{
+		PublicNetworkAccessDisabled,
+		PublicNetworkAccessEnabled,
+		PublicNetworkAccessSecuredByPerimeter,
+	}
+}
+
 // PublicNetworkAccessFlag - This determines if traffic is allowed over public network. By default it is enabled.
 type PublicNetworkAccessFlag string
 
@@ -305,6 +323,24 @@ func PossibleSKUTierValues() []SKUTier {
 		SKUTierBasic,
 		SKUTierPremium,
 		SKUTierStandard,
+	}
+}
+
+// TLSVersion - The minimum TLS version for the cluster to support, e.g. '1.2'
+type TLSVersion string
+
+const (
+	TLSVersionOne0 TLSVersion = "1.0"
+	TLSVersionOne1 TLSVersion = "1.1"
+	TLSVersionOne2 TLSVersion = "1.2"
+)
+
+// PossibleTLSVersionValues returns the possible values for the TLSVersion const type.
+func PossibleTLSVersionValues() []TLSVersion {
+	return []TLSVersion{
+		TLSVersionOne0,
+		TLSVersionOne1,
+		TLSVersionOne2,
 	}
 }
 
