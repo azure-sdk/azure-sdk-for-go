b7dbd8df861f8723e9f943c3c09d1841473f58b5
SDKAuto <sdkautomation@microsoft.com>
2024-09-16 20:30:54 +0000

    CodeGen from PR 30462 in Azure/azure-rest-api-specs Merge f6b07f7a4456a8153d76baa095ae51d75354183f into b751c1c986078c8c9e34dacdfc73d2a581a9c95e

--- sdk/resourcemanager/standbypool/armstandbypool/standbyvirtualmachines_client.go
+++ sdk/resourcemanager/standbypool/armstandbypool/standbyvirtualmachines_client.go
@@ -46,7 +46,7 @@ func NewStandbyVirtualMachinesClient(subscriptionID string, credential azcore.To
 // Get - Get a StandbyVirtualMachineResource
 // If the operation fails it returns an *azcore.ResponseError type.
 //
-// Generated from API version 2023-12-01-preview
+// Generated from API version 2024-03-01
 //   - resourceGroupName - The name of the resource group. The name is case insensitive.
 //   - standbyVirtualMachinePoolName - Name of the standby virtual machine pool
 //   - standbyVirtualMachineName - Name of the standby virtual machine
@@ -98,7 +98,7 @@ func (client *StandbyVirtualMachinesClient) getCreateRequest(ctx context.Context
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2023-12-01-preview")
+	reqQP.Set("api-version", "2024-03-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
@@ -115,7 +115,7 @@ func (client *StandbyVirtualMachinesClient) getHandleResponse(resp *http.Respons
 
 // NewListByStandbyVirtualMachinePoolResourcePager - List StandbyVirtualMachineResource resources by StandbyVirtualMachinePoolResource
 //
-// Generated from API version 2023-12-01-preview
+// Generated from API version 2024-03-01
 //   - resourceGroupName - The name of the resource group. The name is case insensitive.
 //   - standbyVirtualMachinePoolName - Name of the standby virtual machine pool
 //   - options - StandbyVirtualMachinesClientListByStandbyVirtualMachinePoolResourceOptions contains the optional parameters for
@@ -163,7 +163,7 @@ func (client *StandbyVirtualMachinesClient) listByStandbyVirtualMachinePoolResou
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2023-12-01-preview")
+	reqQP.Set("api-version", "2024-03-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
