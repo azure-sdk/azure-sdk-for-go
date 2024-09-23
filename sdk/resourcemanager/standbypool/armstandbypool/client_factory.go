a7bc6c11eb2d37047213766dc951d09303f63d96
SDKAuto <sdkautomation@microsoft.com>
2024-09-23 08:59:20 +0000

    CodeGen from PR 3504 in test-repo-billy/azure-rest-api-specs Merge f515d54057fda70c4f8baad667817b7c8b55c265 into 02e500856ce974e6873e205ffdb6523ffdc60bb7

--- sdk/resourcemanager/standbypool/armstandbypool/client_factory.go
+++ sdk/resourcemanager/standbypool/armstandbypool/client_factory.go
@@ -16,23 +16,20 @@ import (
 // ClientFactory is a client factory used to create any client in this module.
 // Don't use this type directly, use NewClientFactory instead.
 type ClientFactory struct {
-	subscriptionID string
-	internal       *arm.Client
+	internal *arm.Client
 }
 
 // NewClientFactory creates a new instance of ClientFactory with the specified values.
 // The parameter values will be propagated to any client created from this factory.
-//   - subscriptionID - The ID of the target subscription. The value must be an UUID.
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
-func NewClientFactory(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*ClientFactory, error) {
+func NewClientFactory(credential azcore.TokenCredential, options *arm.ClientOptions) (*ClientFactory, error) {
 	internal, err := arm.NewClient(moduleName, moduleVersion, credential, options)
 	if err != nil {
 		return nil, err
 	}
 	return &ClientFactory{
-		subscriptionID: subscriptionID,
-		internal:       internal,
+		internal: internal,
 	}, nil
 }
 
@@ -40,29 +37,5 @@ func NewClientFactory(subscriptionID string, credential azcore.TokenCredential, 
 func (c *ClientFactory) NewOperationsClient() *OperationsClient {
 	return &OperationsClient{
 		internal: c.internal,
-	}
-}
-
-// NewStandbyContainerGroupPoolsClient creates a new instance of StandbyContainerGroupPoolsClient.
-func (c *ClientFactory) NewStandbyContainerGroupPoolsClient() *StandbyContainerGroupPoolsClient {
-	return &StandbyContainerGroupPoolsClient{
-		subscriptionID: c.subscriptionID,
-		internal:       c.internal,
-	}
-}
-
-// NewStandbyVirtualMachinePoolsClient creates a new instance of StandbyVirtualMachinePoolsClient.
-func (c *ClientFactory) NewStandbyVirtualMachinePoolsClient() *StandbyVirtualMachinePoolsClient {
-	return &StandbyVirtualMachinePoolsClient{
-		subscriptionID: c.subscriptionID,
-		internal:       c.internal,
-	}
-}
-
-// NewStandbyVirtualMachinesClient creates a new instance of StandbyVirtualMachinesClient.
-func (c *ClientFactory) NewStandbyVirtualMachinesClient() *StandbyVirtualMachinesClient {
-	return &StandbyVirtualMachinesClient{
-		subscriptionID: c.subscriptionID,
-		internal:       c.internal,
 	}
 }
