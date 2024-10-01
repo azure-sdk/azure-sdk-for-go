368c88fe75bf15f676d8a2bd8b6bf905239b4430
SDKAuto <sdkautomation@microsoft.com>
2024-10-01 23:55:13 +0000

    CodeGen from PR 30824 in Azure/azure-rest-api-specs Merge b2fdf5c0b14033037a6029f1b9701143ac199cdc into e605d439dda724fc2fc0274e0a82a3434a1524d7

--- sdk/resourcemanager/mysql/armmysqlflexibleservers/constants.go
+++ sdk/resourcemanager/mysql/armmysqlflexibleservers/constants.go
@@ -10,7 +10,7 @@ package armmysqlflexibleservers
 
 const (
 	moduleName    = "github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mysql/armmysqlflexibleservers"
-	moduleVersion = "v2.0.0-beta.3"
+	moduleVersion = "v2.0.0-beta.4"
 )
 
 type AdministratorName string
@@ -433,6 +433,22 @@ func PossibleOperationStatusValues() []OperationStatus {
 	}
 }
 
+// PatchStrategy - Enum to indicate the patch strategy of a server
+type PatchStrategy string
+
+const (
+	PatchStrategyRegular       PatchStrategy = "Regular"
+	PatchStrategyVirtualCanary PatchStrategy = "VirtualCanary"
+)
+
+// PossiblePatchStrategyValues returns the possible values for the PatchStrategy const type.
+func PossiblePatchStrategyValues() []PatchStrategy {
+	return []PatchStrategy{
+		PatchStrategyRegular,
+		PatchStrategyVirtualCanary,
+	}
+}
+
 // PrivateEndpointConnectionProvisioningState - The current provisioning state.
 type PrivateEndpointConnectionProvisioningState string
 
@@ -584,5 +600,21 @@ func PossibleServerVersionValues() []ServerVersion {
 	return []ServerVersion{
 		ServerVersionEight021,
 		ServerVersionFive7,
+	}
+}
+
+// StorageRedundancyEnum - Enum to indicate whether storage sku value is 'ZoneRedundancy' or 'LocalRedundancy'
+type StorageRedundancyEnum string
+
+const (
+	StorageRedundancyEnumLocalRedundancy StorageRedundancyEnum = "LocalRedundancy"
+	StorageRedundancyEnumZoneRedundancy  StorageRedundancyEnum = "ZoneRedundancy"
+)
+
+// PossibleStorageRedundancyEnumValues returns the possible values for the StorageRedundancyEnum const type.
+func PossibleStorageRedundancyEnumValues() []StorageRedundancyEnum {
+	return []StorageRedundancyEnum{
+		StorageRedundancyEnumLocalRedundancy,
+		StorageRedundancyEnumZoneRedundancy,
 	}
 }
