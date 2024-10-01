d13384c1284472c768eafeb3a2fd95dd160c1551
SDKAuto <sdkautomation@microsoft.com>
2024-10-01 22:58:32 +0000

    CodeGen from PR 30820 in Azure/azure-rest-api-specs Merge a921d8d9a1640a97adddbd2900688bbfa27465a6 into e605d439dda724fc2fc0274e0a82a3434a1524d7

--- sdk/resourcemanager/securityinsights/armsecurityinsights/threatintelligenceindicators_client.go
+++ sdk/resourcemanager/securityinsights/armsecurityinsights/threatintelligenceindicators_client.go
@@ -29,7 +29,7 @@ type ThreatIntelligenceIndicatorsClient struct {
 }
 
 // NewThreatIntelligenceIndicatorsClient creates a new instance of ThreatIntelligenceIndicatorsClient with the specified values.
-//   - subscriptionID - The ID of the target subscription.
+//   - subscriptionID - The ID of the target subscription. The value must be an UUID.
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
 func NewThreatIntelligenceIndicatorsClient(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*ThreatIntelligenceIndicatorsClient, error) {
@@ -46,7 +46,7 @@ func NewThreatIntelligenceIndicatorsClient(subscriptionID string, credential azc
 
 // NewListPager - Get all threat intelligence indicators.
 //
-// Generated from API version 2022-09-01-preview
+// Generated from API version 2024-04-01-preview
 //   - resourceGroupName - The name of the resource group. The name is case insensitive.
 //   - workspaceName - The name of the workspace.
 //   - options - ThreatIntelligenceIndicatorsClientListOptions contains the optional parameters for the ThreatIntelligenceIndicatorsClient.NewListPager
@@ -106,7 +106,7 @@ func (client *ThreatIntelligenceIndicatorsClient) listCreateRequest(ctx context.
 	if options != nil && options.Top != nil {
 		reqQP.Set("$top", strconv.FormatInt(int64(*options.Top), 10))
 	}
-	reqQP.Set("api-version", "2022-09-01-preview")
+	reqQP.Set("api-version", "2024-04-01-preview")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
