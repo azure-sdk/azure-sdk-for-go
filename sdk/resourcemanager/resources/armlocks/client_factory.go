90955b358ae3ca8fffac480ee38c9f44b3ac4e81
SDKAuto <sdkautomation@microsoft.com>
2024-11-18 08:50:33 +0000

    CodeGen from PR 31507 in Azure/azure-rest-api-specs Merge 89ed321748f7879749f150983cd1b069f5c59e4c into 297c3a012316e290e6a6ab737ec8944611554542

--- sdk/resourcemanager/resources/armlocks/client_factory.go
+++ sdk/resourcemanager/resources/armlocks/client_factory.go
@@ -17,8 +17,7 @@ import (
 // Don't use this type directly, use NewClientFactory instead.
 type ClientFactory struct {
 	subscriptionID string
-	credential     azcore.TokenCredential
-	options        *arm.ClientOptions
+	internal       *arm.Client
 }
 
 // NewClientFactory creates a new instance of ClientFactory with the specified values.
@@ -27,24 +26,27 @@ type ClientFactory struct {
 //   - credential - used to authorize requests. Usually a credential from azidentity.
 //   - options - pass nil to accept the default values.
 func NewClientFactory(subscriptionID string, credential azcore.TokenCredential, options *arm.ClientOptions) (*ClientFactory, error) {
-	_, err := arm.NewClient(moduleName, moduleVersion, credential, options)
+	internal, err := arm.NewClient(moduleName, moduleVersion, credential, options)
 	if err != nil {
 		return nil, err
 	}
 	return &ClientFactory{
-		subscriptionID: subscriptionID, credential: credential,
-		options: options.Clone(),
+		subscriptionID: subscriptionID,
+		internal:       internal,
 	}, nil
 }
 
 // NewAuthorizationOperationsClient creates a new instance of AuthorizationOperationsClient.
 func (c *ClientFactory) NewAuthorizationOperationsClient() *AuthorizationOperationsClient {
-	subClient, _ := NewAuthorizationOperationsClient(c.credential, c.options)
-	return subClient
+	return &AuthorizationOperationsClient{
+		internal: c.internal,
+	}
 }
 
 // NewManagementLocksClient creates a new instance of ManagementLocksClient.
 func (c *ClientFactory) NewManagementLocksClient() *ManagementLocksClient {
-	subClient, _ := NewManagementLocksClient(c.subscriptionID, c.credential, c.options)
-	return subClient
+	return &ManagementLocksClient{
+		subscriptionID: c.subscriptionID,
+		internal:       c.internal,
+	}
 }
