a57d3a4d2ffc616312e4090bee18248e86dbe218
SDKAuto <sdkautomation@microsoft.com>
2024-12-08 21:25:46 +0000

    CodeGen from PR 31662 in Azure/azure-rest-api-specs Merge c0867735df4bf005625e8107bac9d09d6bdc1e19 into ab04533261eff228f28e08900445d0edef3eb70c

--- sdk/resourcemanager/fabric/armfabric/responses.go
+++ sdk/resourcemanager/fabric/armfabric/responses.go
@@ -51,6 +51,12 @@ type CapacitiesClientListSKUsResponse struct {
 	RpSKUEnumerationForNewResourceResult
 }
 
+// CapacitiesClientListUsagesResponse contains the response from method CapacitiesClient.NewListUsagesPager.
+type CapacitiesClientListUsagesResponse struct {
+	// An object that represents enumerating usage and quota details for existing resources
+	RpUsageAndQuotaEnumerationForExistingResourceResult
+}
+
 // CapacitiesClientResumeResponse contains the response from method CapacitiesClient.BeginResume.
 type CapacitiesClientResumeResponse struct {
 	// placeholder for future response values
