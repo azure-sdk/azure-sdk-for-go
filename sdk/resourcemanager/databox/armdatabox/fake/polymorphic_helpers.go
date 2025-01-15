8a521e84e9b983374d2c0410f7d66e64804cba73
SDKAuto <sdkautomation@microsoft.com>
2025-01-15 20:12:45 +0000

    CodeGen from PR 32167 in Azure/azure-rest-api-specs Merge 81e38c4d1bf708de61205561a7289185d921b827 into ca2053cffe88a11dd55c773538a5af305c9a034b

--- sdk/resourcemanager/databox/armdatabox/fake/polymorphic_helpers.go
+++ sdk/resourcemanager/databox/armdatabox/fake/polymorphic_helpers.go
@@ -14,7 +14,7 @@ import (
 )
 
 func unmarshalValidationRequestClassification(rawMsg json.RawMessage) (armdatabox.ValidationRequestClassification, error) {
-	if rawMsg == nil {
+	if rawMsg == nil || string(rawMsg) == "null" {
 		return nil, nil
 	}
 	var m map[string]any
