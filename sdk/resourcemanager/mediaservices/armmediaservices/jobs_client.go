10c98d563759732b242369699ea548034d19c1dc
SDKAuto <sdkautomation@microsoft.com>
2025-01-24 02:43:44 +0000

    CodeGen from PR 32312 in Azure/azure-rest-api-specs Merge f39c04630cbd7b70c5a369f83f5485484cf1683d into 63d36000794ed89006cda8588861a2ae7ed96bdc

--- sdk/resourcemanager/mediaservices/armmediaservices/jobs_client.go
+++ sdk/resourcemanager/mediaservices/armmediaservices/jobs_client.go
@@ -377,13 +377,13 @@ func (client *JobsClient) listCreateRequest(ctx context.Context, resourceGroupNa
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2022-07-01")
 	if options != nil && options.Filter != nil {
 		reqQP.Set("$filter", *options.Filter)
 	}
 	if options != nil && options.Orderby != nil {
 		reqQP.Set("$orderby", *options.Orderby)
 	}
+	reqQP.Set("api-version", "2022-07-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
