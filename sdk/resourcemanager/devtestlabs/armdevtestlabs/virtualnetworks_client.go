e5c64aa40200d7efd5a8ffd1d94f4dc54987e8b8
SDKAuto <sdkautomation@microsoft.com>
2025-01-28 21:23:05 +0000

    CodeGen from PR 32366 in Azure/azure-rest-api-specs Merge 84b2e541bf19d043dd4a38db63e8958ac20c92bf into 175d71800b942faaa4113416d5e395b8ba19ce31

--- sdk/resourcemanager/devtestlabs/armdevtestlabs/virtualnetworks_client.go
+++ sdk/resourcemanager/devtestlabs/armdevtestlabs/virtualnetworks_client.go
@@ -338,11 +338,11 @@ func (client *VirtualNetworksClient) listCreateRequest(ctx context.Context, reso
 	if options != nil && options.Filter != nil {
 		reqQP.Set("$filter", *options.Filter)
 	}
-	if options != nil && options.Top != nil {
-		reqQP.Set("$top", strconv.FormatInt(int64(*options.Top), 10))
-	}
 	if options != nil && options.Orderby != nil {
 		reqQP.Set("$orderby", *options.Orderby)
+	}
+	if options != nil && options.Top != nil {
+		reqQP.Set("$top", strconv.FormatInt(int64(*options.Top), 10))
 	}
 	reqQP.Set("api-version", "2018-09-15")
 	req.Raw().URL.RawQuery = reqQP.Encode()
