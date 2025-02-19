201d6b9bf6f2991c3337b49117a6cfa922e1f246
SDKAuto <sdkautomation@microsoft.com>
2025-02-19 02:46:03 +0000

    CodeGen from PR 32696 in Azure/azure-rest-api-specs Merge 33cdf67d8544cee891f545c10a283fd6723d1ad4 into 91961b8900e8d892b156624e84d941991a54cac7

--- sdk/resourcemanager/advisor/armadvisor/recommendationmetadata_client.go
+++ sdk/resourcemanager/advisor/armadvisor/recommendationmetadata_client.go
@@ -10,14 +10,11 @@ package armadvisor
 
 import (
 	"context"
-	"errors"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
 // RecommendationMetadataClient contains the methods for the RecommendationMetadata group.
@@ -40,65 +37,9 @@ func NewRecommendationMetadataClient(credential azcore.TokenCredential, options 
 	return client, nil
 }
 
-// Get - Gets the metadata entity.
-// If the operation fails it returns an *azcore.ResponseError type.
-//
-// Generated from API version 2020-01-01
-//   - name - Name of metadata entity.
-//   - options - RecommendationMetadataClientGetOptions contains the optional parameters for the RecommendationMetadataClient.Get
-//     method.
-func (client *RecommendationMetadataClient) Get(ctx context.Context, name string, options *RecommendationMetadataClientGetOptions) (RecommendationMetadataClientGetResponse, error) {
-	var err error
-	const operationName = "RecommendationMetadataClient.Get"
-	ctx = context.WithValue(ctx, runtime.CtxAPINameKey{}, operationName)
-	ctx, endSpan := runtime.StartSpan(ctx, operationName, client.internal.Tracer(), nil)
-	defer func() { endSpan(err) }()
-	req, err := client.getCreateRequest(ctx, name, options)
-	if err != nil {
-		return RecommendationMetadataClientGetResponse{}, err
-	}
-	httpResp, err := client.internal.Pipeline().Do(req)
-	if err != nil {
-		return RecommendationMetadataClientGetResponse{}, err
-	}
-	if !runtime.HasStatusCode(httpResp, http.StatusOK) {
-		err = runtime.NewResponseError(httpResp)
-		return RecommendationMetadataClientGetResponse{}, err
-	}
-	resp, err := client.getHandleResponse(httpResp)
-	return resp, err
-}
-
-// getCreateRequest creates the Get request.
-func (client *RecommendationMetadataClient) getCreateRequest(ctx context.Context, name string, options *RecommendationMetadataClientGetOptions) (*policy.Request, error) {
-	urlPath := "/providers/Microsoft.Advisor/metadata/{name}"
-	if name == "" {
-		return nil, errors.New("parameter name cannot be empty")
-	}
-	urlPath = strings.ReplaceAll(urlPath, "{name}", url.PathEscape(name))
-	req, err := runtime.NewRequest(ctx, http.MethodGet, runtime.JoinPaths(client.internal.Endpoint(), urlPath))
-	if err != nil {
-		return nil, err
-	}
-	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2020-01-01")
-	req.Raw().URL.RawQuery = reqQP.Encode()
-	req.Raw().Header["Accept"] = []string{"application/json"}
-	return req, nil
-}
-
-// getHandleResponse handles the Get response.
-func (client *RecommendationMetadataClient) getHandleResponse(resp *http.Response) (RecommendationMetadataClientGetResponse, error) {
-	result := RecommendationMetadataClientGetResponse{}
-	if err := runtime.UnmarshalAsJSON(resp, &result.MetadataEntity); err != nil {
-		return RecommendationMetadataClientGetResponse{}, err
-	}
-	return result, nil
-}
-
 // NewListPager - Gets the list of metadata entities.
 //
-// Generated from API version 2020-01-01
+// Generated from API version 2023-01-01
 //   - options - RecommendationMetadataClientListOptions contains the optional parameters for the RecommendationMetadataClient.NewListPager
 //     method.
 func (client *RecommendationMetadataClient) NewListPager(options *RecommendationMetadataClientListOptions) *runtime.Pager[RecommendationMetadataClientListResponse] {
@@ -132,7 +73,7 @@ func (client *RecommendationMetadataClient) listCreateRequest(ctx context.Contex
 		return nil, err
 	}
 	reqQP := req.Raw().URL.Query()
-	reqQP.Set("api-version", "2020-01-01")
+	reqQP.Set("api-version", "2023-01-01")
 	req.Raw().URL.RawQuery = reqQP.Encode()
 	req.Raw().Header["Accept"] = []string{"application/json"}
 	return req, nil
