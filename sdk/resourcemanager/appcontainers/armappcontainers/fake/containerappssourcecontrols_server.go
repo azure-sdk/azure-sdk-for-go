4f10cdf54be668938917824d3e270ac05c280b92
SDKAuto <sdkautomation@microsoft.com>
2025-02-19 09:47:18 +0000

    CodeGen from PR 32653 in Azure/azure-rest-api-specs Merge 1e67afe31caceb23fa613091812d19bec1eda675 into 659cff5aeaa6fa871bab67f648dd15cea067e4c3

--- sdk/resourcemanager/appcontainers/armappcontainers/fake/containerappssourcecontrols_server.go
+++ sdk/resourcemanager/appcontainers/armappcontainers/fake/containerappssourcecontrols_server.go
@@ -12,16 +12,15 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/http"
-	"net/url"
-	"regexp"
-	"strconv"
-
 	azfake "github.com/Azure/azure-sdk-for-go/sdk/azcore/fake"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/fake/server"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appcontainers/armappcontainers/v3"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
 )
 
 // ContainerAppsSourceControlsServer is a fake server for instances of the armappcontainers.ContainerAppsSourceControlsClient type.
