0be3d25eadbc24ac2672e978349caaf364909966
SDKAuto <sdkautomation@microsoft.com>
2025-03-07 08:37:18 +0000

    CodeGen from PR 32939 in Azure/azure-rest-api-specs Merge bad64037c85b30f22dbd5b64840888dd00154aa9 into af57daeaf232c2a59bf4ba6a522479c265e2002e

--- sdk/resourcemanager/mongocluster/armmongocluster/models.go
+++ sdk/resourcemanager/mongocluster/armmongocluster/models.go
@@ -62,6 +62,12 @@ type ConnectionString struct {
 	Name *string
 }
 
+// DataAPIProperties - Data API properties.
+type DataAPIProperties struct {
+	// The mode to indicate whether the Mongo Data API is enabled for a cluster.
+	Mode *DataAPIMode
+}
+
 // FirewallRule - Represents a mongo cluster firewall rule.
 type FirewallRule struct {
 	// The resource-specific properties for this resource.
@@ -148,11 +154,11 @@ type MongoCluster struct {
 
 // Operation - Details of a REST API operation, returned from the Resource Provider Operations API
 type Operation struct {
-	// Extensible enum. Indicates the action type. "Internal" refers to actions that are for internal only APIs.
-	ActionType *ActionType
-
-	// READ-ONLY; Localized display information for this particular operation.
+	// Localized display information for this particular operation.
 	Display *OperationDisplay
+
+	// READ-ONLY; Extensible enum. Indicates the action type. "Internal" refers to actions that are for internal only APIs.
+	ActionType *ActionType
 
 	// READ-ONLY; Whether the operation applies to data-plane. This is "true" for data-plane operations and "false" for Azure
 	// Resource Manager/control-plane operations.
@@ -337,6 +343,9 @@ type Properties struct {
 	// The mode to create a mongo cluster.
 	CreateMode *CreateMode
 
+	// The Data API properties of the mongo cluster.
+	DataAPI *DataAPIProperties
+
 	// The high availability properties of the mongo cluster.
 	HighAvailability *HighAvailabilityProperties
 
@@ -491,6 +500,9 @@ type UpdateProperties struct {
 
 	// The compute properties of the mongo cluster.
 	Compute *ComputeProperties
+
+	// The Data API properties of the mongo cluster.
+	DataAPI *DataAPIProperties
 
 	// The high availability properties of the mongo cluster.
 	HighAvailability *HighAvailabilityProperties
