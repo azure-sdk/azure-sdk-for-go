4cc73081489c4293d35be56ac3bad34c8a0ef225
SDKAuto <sdkautomation@microsoft.com>
2025-03-26 04:00:26 +0000

    CodeGen from PR 32282 in Azure/azure-rest-api-specs Merge 65f2e0b3ff80b07c858beaa13e834b54e2ecfd44 into b5838c4f5469766558f700d9a6d06ee04ac83750

--- sdk/messaging/eventgrid/aznamespaces/build.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-//go:generate tsp-client update
-//go:generate goimports -w .
-//go:generate go run ./internal/generate
-//go:generate goimports -w .
-
-package aznamespaces
