31fe39c136c1363cab206ec24416ea6cbe26d0d2
SDKAuto <sdkautomation@microsoft.com>
2025-04-16 06:27:50 +0000

    CodeGen from PR 33891 in Azure/azure-rest-api-specs Merge ef5e5267af8a803bf2fa88e777503adae4203c0f into 4f6ba43a83ed4f80863e1fe698ac10b89e8c48a0

--- sdk/resourcemanager/playwrightservice/armplaywrightservice/fake/operations_server.go
+++ sdk/resourcemanager/playwrightservice/armplaywrightservice/fake/operations_server.go
@@ -12,32 +12,32 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/fake/server"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/loadtesting/armloadtesting/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/playwrightservice/armplaywrightservice"
 	"net/http"
 )
 
-// OperationsServer is a fake server for instances of the armloadtesting.OperationsClient type.
+// OperationsServer is a fake server for instances of the armplaywrightservice.OperationsClient type.
 type OperationsServer struct {
 	// NewListPager is the fake for method OperationsClient.NewListPager
 	// HTTP status codes to indicate success: http.StatusOK
-	NewListPager func(options *armloadtesting.OperationsClientListOptions) (resp azfake.PagerResponder[armloadtesting.OperationsClientListResponse])
+	NewListPager func(options *armplaywrightservice.OperationsClientListOptions) (resp azfake.PagerResponder[armplaywrightservice.OperationsClientListResponse])
 }
 
 // NewOperationsServerTransport creates a new instance of OperationsServerTransport with the provided implementation.
-// The returned OperationsServerTransport instance is connected to an instance of armloadtesting.OperationsClient via the
+// The returned OperationsServerTransport instance is connected to an instance of armplaywrightservice.OperationsClient via the
 // azcore.ClientOptions.Transporter field in the client's constructor parameters.
 func NewOperationsServerTransport(srv *OperationsServer) *OperationsServerTransport {
 	return &OperationsServerTransport{
 		srv:          srv,
-		newListPager: newTracker[azfake.PagerResponder[armloadtesting.OperationsClientListResponse]](),
+		newListPager: newTracker[azfake.PagerResponder[armplaywrightservice.OperationsClientListResponse]](),
 	}
 }
 
-// OperationsServerTransport connects instances of armloadtesting.OperationsClient to instances of OperationsServer.
+// OperationsServerTransport connects instances of armplaywrightservice.OperationsClient to instances of OperationsServer.
 // Don't use this type directly, use NewOperationsServerTransport instead.
 type OperationsServerTransport struct {
 	srv          *OperationsServer
-	newListPager *tracker[azfake.PagerResponder[armloadtesting.OperationsClientListResponse]]
+	newListPager *tracker[azfake.PagerResponder[armplaywrightservice.OperationsClientListResponse]]
 }
 
 // Do implements the policy.Transporter interface for OperationsServerTransport.
@@ -93,7 +93,7 @@ func (o *OperationsServerTransport) dispatchNewListPager(req *http.Request) (*ht
 		resp := o.srv.NewListPager(nil)
 		newListPager = &resp
 		o.newListPager.add(req, newListPager)
-		server.PagerResponderInjectNextLinks(newListPager, req, func(page *armloadtesting.OperationsClientListResponse, createLink func() string) {
+		server.PagerResponderInjectNextLinks(newListPager, req, func(page *armplaywrightservice.OperationsClientListResponse, createLink func() string) {
 			page.NextLink = to.Ptr(createLink())
 		})
 	}
