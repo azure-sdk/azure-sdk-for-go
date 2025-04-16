782ce140856fcf86edbe26db595f636d5aa92fb7
SDKAuto <sdkautomation@microsoft.com>
2025-04-16 07:44:23 +0000

    CodeGen from PR 33986 in Azure/azure-rest-api-specs Merge c0d9c680f240997b7a7d0de1eaebd9986988bc1a into 4f6ba43a83ed4f80863e1fe698ac10b89e8c48a0

--- sdk/resourcemanager/connectedvmware/armconnectedvmware/fake/vminstancehybrididentitymetadata_server.go
+++ sdk/resourcemanager/connectedvmware/armconnectedvmware/fake/vminstancehybrididentitymetadata_server.go
@@ -1,6 +1,3 @@
-//go:build go1.18
-// +build go1.18
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -67,23 +64,42 @@ func (v *VMInstanceHybridIdentityMetadataServerTransport) Do(req *http.Request) 
 		return nil, nonRetriableError{errors.New("unable to dispatch request, missing value for CtxAPINameKey")}
 	}
 
-	var resp *http.Response
-	var err error
+	return v.dispatchToMethodFake(req, method)
+}
 
-	switch method {
-	case "VMInstanceHybridIdentityMetadataClient.Get":
-		resp, err = v.dispatchGet(req)
-	case "VMInstanceHybridIdentityMetadataClient.NewListPager":
-		resp, err = v.dispatchNewListPager(req)
-	default:
-		err = fmt.Errorf("unhandled API %s", method)
+func (v *VMInstanceHybridIdentityMetadataServerTransport) dispatchToMethodFake(req *http.Request, method string) (*http.Response, error) {
+	resultChan := make(chan result)
+	defer close(resultChan)
+
+	go func() {
+		var intercepted bool
+		var res result
+		if vmInstanceHybridIdentityMetadataServerTransportInterceptor != nil {
+			res.resp, res.err, intercepted = vmInstanceHybridIdentityMetadataServerTransportInterceptor.Do(req)
+		}
+		if !intercepted {
+			switch method {
+			case "VMInstanceHybridIdentityMetadataClient.Get":
+				res.resp, res.err = v.dispatchGet(req)
+			case "VMInstanceHybridIdentityMetadataClient.NewListPager":
+				res.resp, res.err = v.dispatchNewListPager(req)
+			default:
+				res.err = fmt.Errorf("unhandled API %s", method)
+			}
+
+		}
+		select {
+		case resultChan <- res:
+		case <-req.Context().Done():
+		}
+	}()
+
+	select {
+	case <-req.Context().Done():
+		return nil, req.Context().Err()
+	case res := <-resultChan:
+		return res.resp, res.err
 	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
 }
 
 func (v *VMInstanceHybridIdentityMetadataServerTransport) dispatchGet(req *http.Request) (*http.Response, error) {
@@ -150,4 +166,10 @@ func (v *VMInstanceHybridIdentityMetadataServerTransport) dispatchNewListPager(r
 		v.newListPager.remove(req)
 	}
 	return resp, nil
+}
+
+// set this to conditionally intercept incoming requests to VMInstanceHybridIdentityMetadataServerTransport
+var vmInstanceHybridIdentityMetadataServerTransportInterceptor interface {
+	// Do returns true if the server transport should use the returned response/error
+	Do(*http.Request) (*http.Response, error, bool)
 }
