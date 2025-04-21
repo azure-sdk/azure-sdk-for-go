4c9c546bbc05baa49aa39e9842aeff3fe56e37a6
SDKAuto <sdkautomation@microsoft.com>
2025-04-21 03:20:14 +0000

    CodeGen from PR 33698 in Azure/azure-rest-api-specs Merge d82f6698c1524f8ad485970035643563a8889fb5 into f03aa628f37d2d7e10db8cc16e7471342d3beaaf

--- sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/generateawstemplate_client_example_test.go
+++ sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/generateawstemplate_client_example_test.go
@@ -8,7 +8,7 @@ import (
 	"context"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridconnectivity/armhybridconnectivity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/hybridconnectivity/armhybridconnectivity/v2"
 	"log"
 )
 
@@ -19,11 +19,11 @@ func ExampleGenerateAwsTemplateClient_Post() {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
 	ctx := context.Background()
-	clientFactory, err := armhybridconnectivity.NewClientFactory(cred, nil)
+	clientFactory, err := armhybridconnectivity.NewClientFactory("5ACC4579-DB34-4C2F-8F8C-25061168F342", cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := clientFactory.NewGenerateAwsTemplateClient("5ACC4579-DB34-4C2F-8F8C-25061168F342").Post(ctx, armhybridconnectivity.GenerateAwsTemplateRequest{
+	res, err := clientFactory.NewGenerateAwsTemplateClient().Post(ctx, armhybridconnectivity.GenerateAwsTemplateRequest{
 		ConnectorID: to.Ptr("pnxcfjidglabnwxit"),
 		SolutionTypes: []*armhybridconnectivity.SolutionTypeSettings{
 			{
@@ -39,7 +39,7 @@ func ExampleGenerateAwsTemplateClient_Post() {
 	_ = res
 	// If the HTTP response code is 200 as defined in example definition, your response structure would look as follows. Please pay attention that all the values in the output are fake values for just demo purposes.
 	// res = armhybridconnectivity.GenerateAwsTemplateClientPostResponse{
-	// 	PostResponse: &armhybridconnectivity.PostResponse{
+	// 	GenerateAwsTemplateResponse: &armhybridconnectivity.GenerateAwsTemplateResponse{
 	// 	},
 	// }
 }
